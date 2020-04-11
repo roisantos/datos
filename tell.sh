af70613656318374e69e72b1f36feb75536d3306
roisantos <62201385+roisantos@users.noreply.github.com>
2020-04-11 11:35:03 +0200

    Update tell.sh

--- tell.sh
+++ tell.sh
@@ -21,8 +21,6 @@ cpus=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
 
 for user in $(who | awk '{print $1}' | sort -u)
 do
-    # print other user's CPU usage in parallel but skip own one because
-    # spawning many processes will increase our CPU usage significantly
     if [ "$user" = "$own" ]; then continue; fi
     (top -b -n 1 -u "$user" | awk -v user=$user -v CPUS=$cpus 'NR>7 { sum += $9; } END { print user, sum, sum/CPUS; }') &
     # don't spawn too many processes in parallel
